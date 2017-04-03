43fc224428ae35246ba4a21d40b07d40f4a9676b
Tarassssss <taras.zvinskiy@gmail.com>
2017-04-03 22:34:59 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include "Struct_of_data.h"
+int main()
+{
+	Struct_of_data A('A', 6);
+	A.Print();
+	char a[] = "Abcdefghij";
+	int b[] = { 1,2,3,4,8,9,0 };
+	Stek B(A);
+
+	B.Print();
+	B.Push_back_a_lot(a, b, 10, 7);
+	B.Print();
+	B.Delete(10, 5);
+	B.Print();
+	B.Infosize();
+	_getch();
+	return 0;
+}
